df58f6109d3d36e48862083eed3a710eadc4ea26
Kenny Arlexy <kennyarlexy@gmail.com>
2025-10-31 17:06:25 +0700

    tidy up
    organized some experimental call versions in its own function

--- actor_critic.py
+++ actor_critic.py
@@ -10,48 +10,42 @@ class ActorCritic(keras.Model):
         self.action_size = n_actions
 
         # Shared layers
-        self.one_hot_layer = keras.layers.CategoryEncoding(
+        self.one_hot_layer = layers.CategoryEncoding(
             num_tokens=n_actions+1,
             output_mode='one_hot'
         )
-        self.flatten_layer = keras.layers.Flatten()
-        self.embedding = layers.Embedding(input_dim=n_actions+1, output_dim=3, name='action_embedding', mask_zero=True)
-        self.lstm_1    = layers.LSTM(16, return_sequences=True)
-        self.lstm_2    = layers.LSTM(64)
-        self.conv_1d   = layers.Conv1D(filters=4, kernel_size=3, activation='relu')
-        self.max_pooling = layers.MaxPooling1D(pool_size=2)
+        self.concat        = layers.Concatenate()
+        self.flatten_layer = layers.Flatten()
+        self.embedding     = layers.Embedding(input_dim=n_actions+1, output_dim=3, name='action_embedding', mask_zero=True)
+        self.lstm_1        = layers.LSTM(16, return_sequences=True)
+        self.lstm_2        = layers.LSTM(64)
+        self.conv_1d       = layers.Conv1D(filters=4, kernel_size=3, activation='relu')
+        self.max_pooling   = layers.MaxPooling1D(pool_size=2)
 
         # Actor layers
         self.actor_hidden_1 = layers.Dense(32, activation='relu')
         self.actor_hidden_2 = layers.Dense(32, activation='relu')
-        self.actor_output = layers.Dense(n_actions, activation='softmax')
+        self.actor_output   = layers.Dense(n_actions, activation='softmax')
 
         # Critic layers
         self.critic_hidden_1 = layers.Dense(32, activation='relu')
-        self.critic_hidden_2 = layers.Dense(16, activation='relu')
-        self.critic_output = layers.Dense(1, activation='linear')
+        self.critic_hidden_2 = layers.Dense(32, activation='relu')
+        self.critic_output   = layers.Dense(1, activation='linear')
 
     def call(self, inputs):
+        return self._call_ver_1(inputs)
+    
+    def _call_ver_1(self, inputs):
         action_seq = inputs
 
-        # features = self.embedding(action_seq)
-        features = self.one_hot_layer(action_seq)
-        lstm_features = self.lstm_1(features)
-        lstm_features = self.lstm_2(lstm_features)
-        # features = self.conv_1d(features)
-        # features = self.max_pooling(features)
-        # features = self.lstm_1(features)
+        one_hot_features = self.one_hot_layer(action_seq)
+        lstm_features    = self.lstm_2(one_hot_features)
 
-        # features = self.one_hot_layer(action_seq)
-        features = self.flatten_layer(features)
-
-
-        actor = self.actor_hidden_1(features)
-        actor = self.actor_hidden_2(actor)
+        actor = self.flatten_layer(one_hot_features)
+        actor = self.actor_hidden_1(actor)
         actor = self.actor_output(actor)
 
         critic = self.critic_hidden_1(lstm_features)
-        # critic = self.critic_hidden_2(critic)
         critic = self.critic_output(critic)
 
         return actor, critic
