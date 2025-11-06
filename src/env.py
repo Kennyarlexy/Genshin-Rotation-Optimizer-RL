b4f4dd2a3771e5fb9704ad1a5c09db12a7c2b252
Kenny Arlexy <kennyarlexy@gmail.com>
2025-11-06 19:07:17 +0700

    restructure project tree

--- src/env.py
+++ src/env.py
@@ -1,50 +1,56 @@
-from gymnasium import spaces
 from abc import abstractmethod
 from typing import override
+from pathlib import Path
 import numpy as np
 import subprocess
 import re
 import json
 
-from normalizer import WelfordNormalizer
+SCRIPT_PATH = Path(__file__)
+PROJECT_ROOT = SCRIPT_PATH.parent.parent
 
 
 class GcsimEnv:
     """
     A custom environment following the Gymnasium interface.
     """
-    GCSIM_PATH = "gcsim.exe"
-    CONFIG_FILE_PATH = "config.txt"
-    CONFIG_HEADER_FILE_PATH = "config_header.txt"
-    GCSIM_OUT_FILE_PATH = "gcsim_out.json"
+    GCSIM_PATH = PROJECT_ROOT / "bin" / "gcsim.exe"
     
-    def __init__(self, action_mapping: dict, debug: bool=False, debug_period: int=10) -> None:
-        self.action_mapping = action_mapping
-        
+    def __init__(self, config_file_path: Path, config_header_file_path: Path, gcsim_out_file_path: Path, action_mapping: dict, debug: bool=False, debug_period: int=10) -> None:
         self.debug = debug
         self.debug_period = debug_period
+
+        self.action_mapping = action_mapping
         self.episode_count = 0
-        
         self.n_actions = self.get_n_actions()
         
-        self.config_file = open(self.CONFIG_FILE_PATH, "r+")
-        with open(self.CONFIG_HEADER_FILE_PATH, "r") as config_header_file:
+        self.config_file_path = config_file_path
+        self.gcsim_out_file_path = gcsim_out_file_path
+        self.config_file = open(config_file_path, "r+")
+        with open(config_header_file_path, "r") as config_header_file:
             self.config_header_content = config_header_file.read()
         
         self.config_file_write_position = None
 
     @abstractmethod
-    def reset(self) -> np.ndarray:        
+    def reset(self) -> np.ndarray:
+        """
+        Reset the environment to an initial state and return the initial observation.
+        """
+        
         self.episode_count += 1
         self._reset_config_file()
 
     @abstractmethod
     def step(self, action) -> tuple[np.ndarray, float, bool]:
+        """
+        Execute one time step within the environment.
+        """
         pass
 
     def close(self) -> None:
         """
-        Clean up resources when the environment is closed.
+        Clean up resources when the environment is closed
         """
         if self.config_file:
             self.config_file.close()
@@ -69,17 +75,28 @@ class GcsimEnv:
         
         return cd_duration, rps
 
-    def _run_gcsim(self) -> float:
+    def _run_gcsim(self) -> tuple[float, float, float]:
+        """
+        return a tuple of dmg, duration, dps
+        """
+        
         try:
-            result = subprocess.run([GcsimEnv.GCSIM_PATH, '-out', GcsimEnv.GCSIM_OUT_FILE_PATH], check=True, capture_output=True, text=True)
+            result = subprocess.run([self.GCSIM_PATH, '-c', self.config_file_path, '-out', self.gcsim_out_file_path], check=True, capture_output=True, text=True)
             output = result.stdout
-            match  = re.search(r"in (\d+) dps", output)
-            dps    = match.group(1)
-            
+
+            match_dmg = re.search(r"Average (\d+\.\d+) damage", output)
+            dmg = float(match_dmg.group(1))
+
+            match_duration = re.search(r"over (\d+\.\d+) seconds", output)
+            duration = float(match_duration.group(1))
+
+            match_dps = re.search(r"in (\d+) dps", output)
+            dps = float(match_dps.group(1))
+
             if self.debug is True and self.episode_count % self.debug_period == 0:
                 print(output)
             
-            return float(dps)
+            return dmg, duration, dps
 
         except subprocess.CalledProcessError as e:
             print(f"Error: {e.stderr}")
@@ -105,8 +122,12 @@ class GcsimV1(GcsimEnv):
     GcsimEnv with fixed number of steps per episode
     """
 
+    GCSIM_OUT_FILE_PATH = PROJECT_ROOT / "gcsim_output" / "gcsimv1" / "gcsim_out.json"
+    CONFIG_FILE_PATH = PROJECT_ROOT / "gcsim_config" / "gcsimv1" / "config.txt"
+    CONFIG_HEADER_FILE_PATH = PROJECT_ROOT / "gcsim_config" / "gcsimv1" / "config_header.txt"
+
     def __init__(self, action_mapping: dict, steps_per_episode=30, debug: bool=False, debug_period: int=10):
-        super().__init__(action_mapping, debug, debug_period)
+        super().__init__(self.CONFIG_FILE_PATH, self.CONFIG_HEADER_FILE_PATH, self.GCSIM_OUT_FILE_PATH, action_mapping, debug, debug_period)
 
         self.steps_per_episode = steps_per_episode
         self.state = np.zeros((self.steps_per_episode + 1,), dtype=np.int32)
@@ -115,9 +136,6 @@ class GcsimV1(GcsimEnv):
     
     @override
     def reset(self) -> np.ndarray:
-        """
-        Reset the environment to an initial state and return the initial observation.
-        """
         super().reset()
 
         self.step_count = 0
@@ -127,9 +145,6 @@ class GcsimV1(GcsimEnv):
     
     @override
     def step(self, action: int) -> tuple[np.ndarray, float, bool]:
-        """
-        Execute one time step within the environment.
-        """
         super().step(action)
         
         self.step_count += 1
@@ -144,8 +159,8 @@ class GcsimV1(GcsimEnv):
         
         return self.state, reward, done
     
-    def _compute_reward(self, raw_dps: int, gcsim_out_file_path: str) -> float:
-        raw_reward = raw_dps
+    def _compute_reward(self, dps: float, gcsim_out_file_path: str) -> float:
+        raw_reward = dps
         normalized_reward = raw_reward / 10000
 
         cd_duration, rps = self._analyze_gcsim_out(gcsim_out_file_path)
@@ -156,6 +171,60 @@ class GcsimV1(GcsimEnv):
     @override
     def get_state_dim(self) -> int:
         return self.steps_per_episode + 1
+    
+    @override
+    def step(self, action: int) -> tuple[np.ndarray, float, bool]:
+        super().step(action)
+        
+        self.step_count += 1
+        self.state[self.step_count] = action
+        done = self.step_count == self.steps_per_episode
+
+        reward = 0.0
+        self._update_config_file(action)
+        if done:
+            _, _, dps = self._run_gcsim()
+            reward = self._compute_reward(dps, self.GCSIM_OUT_FILE_PATH)
+        
+        return self.state, reward, done
+
+
+class GcsimV2(GcsimEnv):
+    """
+    GcsimEnv with fixed duration in seconds
+    """
+    
+    def __init__(self, action_mapping: dict, duration: float=30, debug: bool=False, debug_period: int=10):
+        super().__init__(action_mapping, debug, debug_period)
+        
+        self.duration = duration
+        self.state = np.zeros((150,), dtype=np.int32)
+        self.state[0] = self.n_actions + 1 # the <start> token
+        self.step_count = 0
+
+    @override
+    def reset(self) -> np.ndarray:
+        super().reset()
+
+        self.step_count = 0
+        self.state[1:] = 0
+
+        return self.state
+        
+    @override
+    def step(self, action: int) -> tuple[np.ndarray, float, bool]:
+        super().step(action)
+
+        dmg, duration, _ = self._run_gcsim()
+        
+        self.step_count += 1
+        self.state[self.step_count] = action
+        done = duration == self.duration
+
+        reward = 0.0
+        self._update_config_file(action)
+        
+        return self.state, reward, done
 
 if __name__ == "__main__":
     action_mapping = {
@@ -169,6 +238,7 @@ if __name__ == "__main__":
 
     state = env.reset()
     state_, reward, done = env.step(4)
+    env._run_gcsim()
     print(reward)
     
     env.close()
